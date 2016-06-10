2902603e1dd85878862c0aa1955915e55bcb2d51
Duong Nguyen <nhduong29@gmail.com>
2016-06-10 08:44:31 +0700

    customer page

--- app/customer/customerService.js
+++ app/customer/customerService.js
@@ -5,6 +5,30 @@ angular
             return $http.get('http://greenlightgv.dyndns.org:8080/customer');
         };
         this.aadCustomer = function(customer){
+            var newCustomer = this.addCustomerDefaultValue(customer);
             return $http.post('http://greenlightgv.dyndns.org:8080/customer/',customer);
         }
+
+        this.addCustomerDefaultValue = function(customer){
+            customer.account.active = true;//Default value
+            if(customer.account.role === null){
+                customer.account.paymentPlan = "Economic"; //Default value
+            }
+            if(customer.sex === 'male'){
+                customer.sex =  true;
+            }else{
+                customer.sex = false;
+            }
+            if(customer.birthday === ''){
+                customer.birthday = null;
+            }
+            if(customer.phone === ''){
+                customer.phone = null;
+            }
+            return customer;
+        }
+
+        this.getPaymentPlan = function() {
+            return $http.get('http://greenlightgv.dyndns.org:8080/plan')
+        };
     });
---
+++ js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by DuongHanh on 6/10/2016.
+ */
+$(document).ready(function(){
+    $(".datepicker").datepicker({format: 'yyyy-mm-dd'});
+    $("#birthday").datepicker(); // Sample
+
+});
